a4617060c38b88f327bfad143d783ddc9859d288
cupsos <cupsos@gmail.com>
2022-10-26 17:58:26 +0900

    Refactor class IEnumerableHelper
    Sync file name -> class name
    Change access modifiers of IEnumerableHelper. public -> internal
    Hide class if TFM has System.Memory

---
+++ src/MiniExcel/Utils/IEnumerableHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace MiniExcelLibs.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+#if !(NETSTANDARD2_1 || NETCOREAPP2_1_OR_GREATER)
+    internal static class IEnumerableHelper
+    {
+        public static bool StartsWith<T>(this IEnumerable<T> span, ICollection<T> value) where T : IEquatable<T>
+        {
+            return span.Take(value.Count).SequenceEqual(value);
+        }
+    }
+#endif
+}
